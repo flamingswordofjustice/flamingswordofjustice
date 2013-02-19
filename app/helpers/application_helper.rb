b4f5eb3d1e8ca20d679d98d198a0b7413e6776cf
Brian Guthrie <btguthrie@gmail.com>
2013-02-18 22:59:13 -0500

    Attempting to integrate Livefyre. Externalizing several IDs into env variables.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,16 +11,32 @@ module ApplicationHelper
     HTML
   end
 
-  def twitter_big_follow
-    raw <<-HTML
-      <a href="https://twitter.com/flamingjustice" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @flamingjustice</a>
-      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-    HTML
-  end
-
   def facebook_follow
     raw <<-HTML
       <div class="fb-follow" data-href="https://www.facebook.com/flamingswordofjustice" data-show-faces="false" data-layout="button_count"></div>
+    HTML
+  end
+
+  def livefyre_comments
+    raw <<-HTML
+    <div id="livefyre-comments"></div>
+      <script type="text/javascript" src="http://zor.livefyre.com/wjs/v3.0/javascripts/livefyre.js"></script>
+      <script type="text/javascript">
+      (function () {
+          var articleId = fyre.conv.load.makeArticleId(null);
+          fyre.conv.load({}, [{
+              el: 'livefyre-comments',
+              network: "livefyre.com",
+              siteId: "#{ENV['LIVEFYRE_SITE_ID']}",
+              articleId: articleId,
+              signed: false,
+              collectionMeta: {
+                  articleId: articleId,
+                  url: fyre.conv.load.makeCollectionUrl(),
+              }
+          }], function() {});
+      }());
+      </script>
     HTML
   end
 
@@ -31,13 +47,13 @@ module ApplicationHelper
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=158508567636870";
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=#{ENV['FACEBOOK_APP_ID']}";
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));</script>
     HTML
   end
 
   def itunes_link
-    "https://itunes.apple.com/us/podcast/flaming-sword-justice-ben/id497260543"
+    ENV['ITUNES_URI']
   end
 end
