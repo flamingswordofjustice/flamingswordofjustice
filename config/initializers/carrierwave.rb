163f411310aa0e9b756eaa3c841701b69fada386
Brian Guthrie <btguthrie@gmail.com>
2013-02-20 16:01:37 -0500

    Removing Rails env from Fog directory in an attempt to get around S3 cert errors.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,6 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
     :aws_secret_access_key  => ENV['AWS_SECRET_KEY']
   }
-  config.fog_directory  = "flamingswordofjustice.#{Rails.env}"
+
+  config.fog_directory  = "flamingswordofjustice"
 end
