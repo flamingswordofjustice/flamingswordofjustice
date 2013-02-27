ceed3ec703f389037efd90c2c0404a8ebda6f2d1
Brian Guthrie <btguthrie@gmail.com>
2013-02-27 07:50:55 -0500

    Adding Google Analytics tracking.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -66,6 +66,24 @@ module ApplicationHelper
     HTML
   end
 
+  def google_analytics_tracker
+    raw <<-HTML
+      <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', '#{google_analytics_tracking_id}']);
+        _gaq.push(['_setDomainName', 'flamingswordofjustice.com']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+      </script>
+    HTML
+  end
+
   def itunes_link
     ENV['ITUNES_URI']
   end
@@ -84,6 +102,10 @@ module ApplicationHelper
     content_for :head do
       render partial: 'shared/open_graph', locals: attrs
     end
+  end
+
+  def google_analytics_tracking_id
+    ENV['GOOGLE_ANALYTICS_TRACKING_ID']
   end
 
   def facebook_admin_id
