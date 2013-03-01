484e4d2f83b44838c710cff04787cc1434436789
Brian Guthrie <btguthrie@gmail.com>
2013-03-01 10:19:11 -0500

    Tweaks to the episode admin page to make it easer to navigate.

--- app/admin/episode.rb
+++ app/admin/episode.rb
@@ -1,10 +1,10 @@
 ActiveAdmin.register Episode do
   index do
-    column :title
+    column "Name", :title
     column :published_at
-    column :guests
-    column :description
-    column :topics
+    column(:guests) { |e| e.guests.map {|g| link_to g.name, edit_admin_person_path(g.slug) }.join(', ').html_safe }
+    column(:topics) { |e| e.topics.map {|t| link_to t.name, edit_admin_topic_path(t.slug) }.join(', ').html_safe }
+    column(:description, sortable: :description) { |e| strip_tags e.description }
     default_actions
   end
 
@@ -12,13 +12,13 @@ ActiveAdmin.register Episode do
 
   form html: { multipart: true } do |f|
     f.inputs "Episode Details" do
-      f.input :title
+      f.input :title, label: "Name"
       f.input :download_url
       f.input :description, as: :html_editor
       f.input :published_at, as: :date_select
       f.input :topics
-      f.input :image, as: :file
       f.input :guests
+      f.input :image, as: :file
     end
     f.actions
   end
