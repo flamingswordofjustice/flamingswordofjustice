82b1ac46081205ff38125d8446d2fe8baec93a9c
Brian Guthrie <btguthrie@gmail.com>
2013-02-28 22:33:56 -0500

    Explicitly specifying indices to search on in Elasticsearch.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 
   def index
-    @results = Tire.search do |s|
+    @results = Tire.search('episodes,posts,people') do |s|
       s.query { |q| q.string params[:q] }
     end.results
   end
