59d9d5f654e2dcf7d4bbda93a8244583dc134ab1
Brian Guthrie <btguthrie@gmail.com>
2013-03-12 14:05:45 -0400

    Attempting to provide a default image path.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,13 +15,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :fog
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  # Provide a default URL as a default if there hasn't been a file uploaded.
+  def default_url
+    '/assets/transparent.png'
+  end
 
   # We won't need bigger, and this seems to be what Instagram/FB display.
   process :resize_to_fit => [612,612]
