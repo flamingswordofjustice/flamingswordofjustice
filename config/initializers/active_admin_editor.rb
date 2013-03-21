88533cbcf4bd7a8ca5492f9b227cf62fdab243a1
Brian Guthrie <btguthrie@gmail.com>
2013-03-21 12:46:33 -0400

    Updating parser rules to support embedded YouTube iframes.

--- config/initializers/active_admin_editor.rb
+++ config/initializers/active_admin_editor.rb
@@ -3,4 +3,16 @@ ActiveAdmin::Editor.configure do |config|
   # config.aws_access_key_id = ''
   # config.aws_access_secret = ''
   # config.storage_dir = 'uploads'
+
+  config.parser_rules['tags']['iframe'] = {
+    'remove' => 0,
+    'check_attributes' => {
+      'width' => 'numbers',
+      'height' => 'numbers',
+      'src' => 'url',
+      'frameborder' => 'numbers',
+      'allowfullscreen' => ''
+    }
+  }
+  config.parser_rules['tags']['embed']  = { 'remove' => 0 }
 end
