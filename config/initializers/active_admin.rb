980d36479c098978173ba902c7d38d07d2f18474
Brian Guthrie <btguthrie@gmail.com>
2013-04-01 12:00:28 -0400

    Trying to see if this will work if we mark them as precompiled, as with ActiveAdmin HTML editor.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -141,8 +141,8 @@ ActiveAdmin.setup do |config|
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
 
-  # config.register_javascript 'vendor/jquery.chosen.js'
-  # config.register_stylesheet 'vendor/chosen.css'
+  config.register_javascript 'vendor/jquery.chosen.js'
+  config.register_stylesheet 'vendor/chosen.css'
 
 
   # == CSV options
