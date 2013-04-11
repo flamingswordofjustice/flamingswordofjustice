08009f577ebd02e509f189747a7bdf2fff447de7
Brian Guthrie <btguthrie@gmail.com>
2013-04-11 14:34:36 -0400

    JSON representation of episodes, for use in future episode notes updates.

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -2,6 +2,11 @@ class EpisodesController < ApplicationController
 
   def show
     @episode = Episode.where(slug: params[:id]).first or raise ActiveRecord::RecordNotFound
+
+    respond_to do |f|
+      f.html { }
+      f.json { render json: @episode.to_json(only: [:title, :state, :show_notes, :description]) }
+    end
   end
 
   def latest
