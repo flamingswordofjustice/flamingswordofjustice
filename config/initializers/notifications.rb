14b2efd3ac3a17c91ad5237a9e4ad0e2ccfc9015
Brian Guthrie <btguthrie@gmail.com>
2013-04-17 17:52:59 -0400

    Explicitly using count rather than increment to see if we get better results.

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -10,13 +10,14 @@ ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*a
   ActiveSupport::Notifications.instrument :performance, :action => :timing, :measurement => "#{key}.total_duration", :value => event.duration.to_i
   ActiveSupport::Notifications.instrument :performance, :action => :timing, :measurement => "#{key}.db_time", :value => event.payload[:db_runtime]
   ActiveSupport::Notifications.instrument :performance, :action => :timing, :measurement => "#{key}.view_time", :value => event.payload[:view_runtime]
-  ActiveSupport::Notifications.instrument :performance, :measurement => "#{key}.status.#{status}"
+  ActiveSupport::Notifications.instrument :performance, :action => :count,  :measurement => "#{key}.status.#{status}"
 end
 
 ActiveSupport::Notifications.subscribe /performance/ do |name, start, finish, id, payload|
   action = payload[:action] || :increment
   measurement = payload[:measurement]
-  value = payload[:value]
+  value = payload[:value] || 1
   key_name = "#{name.to_s.capitalize}.#{measurement}"
-  Fsj.statsd.__send__ action.to_s, key_name, (value || 1)
+
+  Fsj.statsd.__send__ action.to_s, key_name, value
 end
