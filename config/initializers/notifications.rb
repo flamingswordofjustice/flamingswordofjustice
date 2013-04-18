e63f91683d1043a2d29a22bc7e308d6b01141506
Brian Guthrie <btguthrie@gmail.com>
2013-04-18 09:53:02 -0400

    Removed capitalization, indented for aesthetics.

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -5,7 +5,7 @@ ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*a
   format = event.payload[:format] || "all"
   format = "all" if format == "*/*"
   status = event.payload[:status]
-  key = "#{controller}.#{action}.#{format}"
+  key    = "#{controller}.#{action}.#{format}"
 
   ActiveSupport::Notifications.instrument :performance, :action => :timing, :measurement => "#{key}.total_duration", :value => event.duration.to_i
   ActiveSupport::Notifications.instrument :performance, :action => :timing, :measurement => "#{key}.db_time", :value => event.payload[:db_runtime]
@@ -14,10 +14,10 @@ ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*a
 end
 
 ActiveSupport::Notifications.subscribe /performance/ do |name, start, finish, id, payload|
-  action = payload[:action] || :increment
+  action      = payload[:action] || :increment
   measurement = payload[:measurement]
-  value = payload[:value] || 1
-  key_name = "#{name.to_s.capitalize}.#{measurement}"
+  value       = payload[:value] || 1
+  key_name    = "#{name}.#{measurement}"
 
   Fsj.statsd.__send__ action.to_s, key_name, value
 end
