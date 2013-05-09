ee5b7f8bbd10552c7f2b2066a01148d91280b75e
Brian Guthrie <btguthrie@gmail.com>
2013-05-09 11:14:17 -0400

    Added Chartbeat tracking.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -83,6 +83,37 @@ module ApplicationHelper
     HTML
   end
 
+  def chartbeat_head_tracker
+    raw <<-HTML
+      <script type="text/javascript">var _sf_startpt=(new Date()).getTime()</script>
+    HTML
+  end
+
+  def chartbeat_tail_tracker
+    raw <<-HTML
+      <script type="text/javascript">
+        var _sf_async_config = { uid: #{chartbeat_uid}, domain: 'flamingswordofjustice.com', useCanonical: true };
+        (function() {
+          function loadChartbeat() {
+            window._sf_endpt = (new Date()).getTime();
+            var e = document.createElement('script');
+            e.setAttribute('language', 'javascript');
+            e.setAttribute('type', 'text/javascript');
+            e.setAttribute('src','//static.chartbeat.com/js/chartbeat.js');
+            document.body.appendChild(e);
+          };
+          var oldonload = window.onload;
+          window.onload = (typeof window.onload != 'function') ?
+            loadChartbeat : function() { oldonload(); loadChartbeat(); };
+        })();
+      </script>
+    HTML
+  end
+
+  def chartbeat_uid
+    ENV['CHARTBEAT_UID']
+  end
+
   def google_analytics_tracker
     raw <<-HTML
       <script type="text/javascript">
