730e40a9ae3d23f2fcb85015f5bbbd2069be61eb
Brian Guthrie <btguthrie@gmail.com>
2013-07-30 10:24:14 -0400

    Disabling Jplayer .swf file until Heroku errors can be resolved.

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -3,7 +3,7 @@ module EpisodesHelper
     {
       "mp3-uri"      => audio_episode_url(episode),
       "tracking-uri" => listener_tracking_uri,
-      "swf-path"     => asset_path("vendor/Jplayer.swf"),
+      # "swf-path"     => asset_path("vendor/Jplayer.swf"),
       "user-session" => session[:session_id],
       "play-session" => SecureRandom.urlsafe_base64
     }
