6b191bf630dac25d22468221fb36bfad3a83902c
Brian Guthrie <btguthrie@gmail.com>
2013-08-27 14:29:51 -0400

    Person pages look ok; still need social icons.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,6 +2,14 @@ class PeopleController < ApplicationController
 
   def show
     @person = Person.find(params[:id])
+
+    if layout.blank?
+      redirect_to person_path(params[:id], params.merge(l: choose_layout))
+    elsif layout == ALTERNATE
+      render template: "people/alt", layout: "minimal"
+    else # Standard layout
+      render template: "people/show", layout: "application"
+    end
   end
 
   def index
