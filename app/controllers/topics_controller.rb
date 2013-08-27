c02b907b37afea7ad6d148a05d85856358030e14
Brian Guthrie <btguthrie@gmail.com>
2013-08-27 12:40:48 -0400

    Added basic topic show page; more to be done here.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,6 +6,14 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
+
+    if layout.blank?
+      redirect_to topic_path(params[:id], params.merge(l: choose_layout))
+    elsif layout == ALTERNATE
+      render template: "topics/alt", layout: "minimal"
+    else # Standard layout
+      render template: "topics/show", layout: "application"
+    end
   end
 
 end
