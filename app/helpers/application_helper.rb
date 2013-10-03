d1421eed11c4a8400762f5f345d77ff0d88650de
Brian Guthrie <btguthrie@gmail.com>
2013-10-03 16:10:31 -0400

    Attempting to clean up Twitter and Livefyre script loads.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,6 @@ module ApplicationHelper
 
     raw <<-HTML
       <a href="https://twitter.com/#{account}" class="twitter-follow-button" data-show-count="#{count}" data-size="small" data-show-screen-name="false">Follow @#{account}</a>
-      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     HTML
   end
 
@@ -25,7 +24,6 @@ module ApplicationHelper
 
     raw <<-HTML
       <a href="https://twitter.com/share" class="twitter-share-button" data-via="#{account}" data-text="#{text}" data-url="#{url}" data-counturl="#{url}">Tweet</a>
-      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     HTML
   end
 
@@ -39,9 +37,13 @@ module ApplicationHelper
 
   def facebook_follow(account=nil)
     account ||= t(:facebook)
-    raw <<-HTML
-      <div class="fb-follow" data-href="https://www.facebook.com/#{account}" data-show-faces="false" data-layout="button_count"></div>
-    HTML
+    location = "https://www.facebook.com/#{account}"
+
+    content_tag :div, "",
+      "class" => "fb-follow",
+      "data-href" => location,
+      "data-show-faces" => "false",
+      "data-layout" => "button_count"
   end
 
   def facebook_like(url_to_like=nil)
@@ -50,32 +52,13 @@ module ApplicationHelper
       url_to_like = "https://www.facebook.com/#{url_to_like}"
     end
 
-    raw <<-HTML
-      <div class="fb-like" data-href="#{url_to_like}" data-send="false" data-width="120" data-layout="button_count" data-show-faces="false"></div>
-    HTML
-  end
-
-  def livefyre_comments
-    raw <<-HTML
-    <div id="livefyre-comments"></div>
-      <script type="text/javascript" src="//zor.livefyre.com/wjs/v3.0/javascripts/livefyre.js"></script>
-      <script type="text/javascript">
-      (function () {
-          var articleId = fyre.conv.load.makeArticleId(null);
-          fyre.conv.load({}, [{
-              el: 'livefyre-comments',
-              network: "livefyre.com",
-              siteId: "#{ENV['LIVEFYRE_SITE_ID']}",
-              articleId: articleId,
-              signed: false,
-              collectionMeta: {
-                  articleId: articleId,
-                  url: fyre.conv.load.makeCollectionUrl(),
-              }
-          }], function() {});
-      }());
-      </script>
-    HTML
+    content_tag :div, "",
+      "class" => "fb-like",
+      "data-href" => url_to_like,
+      "data-send" => "false",
+      "data-width" => 120,
+      "data-layout" => "button_count",
+      "data-show-faces" => "false"
   end
 
   def chartbeat_head_tracker
@@ -307,10 +290,6 @@ module ApplicationHelper
 
   def grouped(title, groups, crumbs)
     render partial: "shared/grouped", locals: { title: title, groups: groups, crumbs: crumbs }
-  end
-
-  def comments_enabled?
-    !Rails.env.development?
   end
 
   def subscribe_join_social(opts={})
