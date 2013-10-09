3f58e47362c1af3147c82486dd6234eea85a1cca
Brian Guthrie <btguthrie@gmail.com>
2013-10-09 11:12:28 -0400

    Fix to layout for generic pages.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def show
     @page = Page.find(params[:page])
     not_found if @page.nil?
+    render template: "pages/alt", layout: "minimal"
   end
 
 end
