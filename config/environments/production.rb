1a4281bf7aef9b65075b710bd10257d2f28ab382
Brian Guthrie <btguthrie@gmail.com>
2013-10-28 21:56:37 -0400

    Use Mailgun for SMTP settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,13 +56,12 @@ Fsj::Application.configure do
 
   # Until we get set up with something a bit more robust.
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'baci.lindsaar.net',
-    :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => 'plain',
-    :enable_starttls_auto => true
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         ENV['PRODUCTION_HOSTNAME'].gsub(/www/, ''),
+    authentication: 'plain'
   }
 
   # Disable delivery errors, bad email addresses will be ignored
